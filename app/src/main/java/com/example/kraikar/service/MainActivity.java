2dbea80b8375d09264a40b07708af15c71307942
Kiran Raikar <kraikar@brocade.com>
2016-12-31 19:32:12 +0530

    Added code to start the Service in Main Activity

--- app/src/main/java/com/example/kraikar/service/MainActivity.java
+++ app/src/main/java/com/example/kraikar/service/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, MyService.class);
+        startService(intent);
     }
 
     public void buttonStartService(View view) {
